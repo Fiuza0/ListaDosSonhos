c2a5579f9d22e59b0683ba80cdf63b2dc890fae5
Fiuza0 <rodrigo.oliveira@ucsal.edu.br>
2021-06-10 20:50:58 -0300

    Implementação da API do IMDb

--- app/src/main/java/com/ucsal/listadossonhos/activities/PlaylistActivity.java
+++ app/src/main/java/com/ucsal/listadossonhos/activities/PlaylistActivity.java
@@ -15,22 +15,28 @@ import com.ucsal.listadossonhos.viewModel.PlaylistViewModel;
 
 import java.util.List;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
 public class PlaylistActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.lista_playlist_recycler_view);
+        RecyclerView rvListaPlaylist = findViewById(R.id.lista_playlist_recycler_view);
         PlaylistViewModel model = new ViewModelProvider(this).get(PlaylistViewModel.class);
-
+        HttpResponse<String> response = Unirest.get("https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json")
+                .header("x-rapidapi-key", "bc76699347mshc7b99201b5e138dp1c96eejsn68b9da1eb7e0")
+                .header("x-rapidapi-host", "movie-database-imdb-alternative.p.rapidapi.com")
+                .asString();
         List<Playlist> listaPlaylist = model.playlists;
 
         ItemPlaylistAdapter adapter = new ItemPlaylistAdapter(listaPlaylist);
 
-        rvContacts.setAdapter(adapter);
+        rvListaPlaylist.setAdapter(adapter);
 
-        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        rvListaPlaylist.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
